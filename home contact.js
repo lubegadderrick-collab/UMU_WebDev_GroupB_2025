b26f323ab6c647d069c7015a5cc84a1ccc53e0a8
lubegadderrick-collab <lubegadderrick@gmail.com>
2025-11-30 19:09:07 +0300

    exam Group B

--- home contact.js
+++ home contact.js
@@ -1,15 +1,15 @@
 // Mobile Navigation Toggle
-const hamburger = document.querySelector('.hamburger');
+const mobilenav = document.querySelector('.mobilenav');
 const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
+mobilenav.addEventListener('click', () => {
+    mobilenav.classList.toggle('active');
     navMenu.classList.toggle('active');
 });
 
 // Close mobile menu when clicking on a link
 document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
-    hamburger.classList.remove('active');
+    mobilenav.classList.remove('active');
     navMenu.classList.remove('active');
 }));
 
